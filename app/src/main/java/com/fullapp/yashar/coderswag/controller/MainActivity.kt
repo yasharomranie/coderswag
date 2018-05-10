fde5bb883b7d00a8361e502760d48676e1361b85
yasharomranie26 <yasharomranie26@gmail.com>
2018-05-10 18:34:33 +0430

    recycle import in to project

--- app/src/main/java/com/fullapp/yashar/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/fullapp/yashar/coderswag/controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fullapp.yashar.coderswag.controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.fullapp.yashar.coderswag.Adapters.CategoryAdapter
 import com.fullapp.yashar.coderswag.R
 import com.fullapp.yashar.coderswag.model.Category
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         adapter = CategoryAdapter(this,Dataservice.category)
         catelist.adapter =adapter
+        catelist.setOnItemClickListener { adapterView, view, i, l ->
+            val catgory =Dataservice.category[i]
+            Toast.makeText(this ,"شما بر روی ${catgory.title} کلیک کزده اید",Toast.LENGTH_LONG).show()
+        }
     }
 }
